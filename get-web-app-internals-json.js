fead5029be438ddae135de8e12a8a5054fe074ab
guest271314 <guest271314@gmail.com>
2025-07-28 05:58:57 +0000

    Update get-web-app-internals-json.js

--- get-web-app-internals-json.js
+++ get-web-app-internals-json.js
@@ -1,22 +1,19 @@
-const targetNode = document.body;
+const targetNode = document.documentElement;
 const config = { childList: true, subtree: true };
-
 const callback = (mutationList, observer) => {
   for (const mutation of mutationList) {
     if (mutation.type === "childList") {
+      console.log(mutation);
       if (mutation.target.id === "json") {
-        scheduler.postTask(() => {
           const json = JSON.parse(mutation.target.textContent);
           console.log(json);
           chrome.runtime.sendMessage(
             json,
           );
           observer.disconnect();
-        }, { priority: "background", delay: 500 });
       }
     }
   }
 };
-
 const observer = new MutationObserver(callback);
 observer.observe(targetNode, config);
