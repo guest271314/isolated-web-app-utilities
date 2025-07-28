a7e8cf3cc4630673567347259e9c79d94fc17734
guest271314 <guest271314@gmail.com>
2025-07-28 02:38:59 +0000

    Update get-web-app-internals-json.js

--- get-web-app-internals-json.js
+++ get-web-app-internals-json.js
@@ -1,3 +1,22 @@
-chrome.runtime.sendMessage(
-  JSON.parse(document.getElementById("json").textContent),
-);
+const targetNode = document.body;
+const config = { childList: true, subtree: true };
+
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === "childList") {
+      if (mutation.target.id === "json") {
+        scheduler.postTask(() => {
+          const json = JSON.parse(mutation.target.textContent);
+          console.log(json);
+          chrome.runtime.sendMessage(
+            json,
+          );
+          observer.disconnect();
+        }, { priority: "background", delay: 500 });
+      }
+    }
+  }
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
