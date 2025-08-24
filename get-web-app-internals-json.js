2d34f49c07d67250ad88d223bf009e9bca3e72af
guest271314 <guest271314@gmail.com>
2025-08-24 04:54:09 +0000

    Update get-web-app-internals-json.js
    Use scheduler.postTask(), MutationObserver does not consistently work on first attempt to open window

--- get-web-app-internals-json.js
+++ get-web-app-internals-json.js
@@ -1,19 +1,9 @@
-const targetNode = document.documentElement;
-const config = { childList: true, subtree: true };
-const callback = (mutationList, observer) => {
-  for (const mutation of mutationList) {
-    if (mutation.type === "childList") {
-      console.log(mutation);
-      if (mutation.target.id === "json") {
-          const json = JSON.parse(mutation.target.textContent);
-          console.log(json);
-          chrome.runtime.sendMessage(
-            json,
-          );
-          observer.disconnect();
-      }
-    }
-  }
-};
-const observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
+scheduler.postTask(() => {
+  const json = JSON.parse(document.getElementById("json").textContent);
+  // console.log(json);
+  chrome.runtime.sendMessage(
+    json,
+  );
+}, {
+  delay: 1000
+});
